8c60db718934391477ec4d19ede2287b760c6d88
Joe Buschmann <joseph.buschmann@outlook.com>
2017-10-16 17:33:34 -0500

    Added initial code

---
+++ src/NUnit3WithGlobalTestGenerator.SpecFlowPlugin/NUnit3WithGlobalTestGeneratorPlugin.cs
@@ -0,0 +1,21 @@
+﻿using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Infrastructure;
+
+[assembly: GeneratorPlugin(typeof(NUnit3WithGlobalTestGenerator.SpecFlowPlugin.NUnit3WithGlobalTestGeneratorPlugin))]
+
+namespace NUnit3WithGlobalTestGenerator.SpecFlowPlugin
+{
+    public class NUnit3WithGlobalTestGeneratorPlugin : IGeneratorPlugin
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters)
+        {
+            generatorPluginEvents.CustomizeDependencies += (sender, args) =>
+            {
+                args.ObjectContainer
+                    .RegisterTypeAs<NUnit3WithGlobalTestGeneratorProvider, IUnitTestGeneratorProvider>();
+            };
+        }
+    }
+}
+
---
+++ src/Tests.NUnit/TestCodeGeneration.feature
@@ -0,0 +1,26 @@
+﻿@ignore
+Feature: Test Code Generation
+	In order to avoid namespace clashes
+	As a user of SpecFlow with NUnit
+	I want namespace to be preceded with the "global" keyword in the generated tests
+
+Background:
+	Given a valid calculator
+
+@ignore
+Scenario: Simple Scenario
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	Then the result should be 120 on the screen
+
+Scenario Outline: Scenario Outline
+	Given I have entered <addend1> into the calculator
+	And I have entered <addend2> into the calculator
+	When I press add
+	Then the result should be <sum> on the screen
+
+	Examples: 
+		| addend1 | addend2 | sum |
+		| 50      | 70      | 120 |
+		| 60      | 70      | 130 |
